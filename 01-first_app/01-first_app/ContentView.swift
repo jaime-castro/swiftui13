7b42fcbcf058994a58871f20bca313ae5640c65e
Jaime <jaime.castro5075@gmail.com>
2020-12-28 19:42:05 -0600

    Cambio color de los textos

---
+++ 01-first_app/01-first_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01-first_app
+//
+//  Created by Lorenzo Jaime Castro Vazquez1 on 28/12/20.
+//  Copyright © 2020 Lorenzo Jaime Castro Vazquez1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hola Mundo Nuevo")
+            .fontWeight(.bold)
+            .font(.system(.largeTitle, design: .serif))
+            .foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
