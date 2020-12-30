3a07a46a41964850ab2015140ea91198497a26bc
Jaime <jaime.castro5075@gmail.com>
2020-12-29 19:35:02 -0600

    app 1 cpmpletada

--- 01-first_app/01-first_app/ContentView.swift
+++ 01-first_app/01-first_app/ContentView.swift
@@ -10,13 +10,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hola Mundo Nuevo")
+        Text("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años.")
             .fontWeight(.bold)
-            .font(.system(.largeTitle, design: .serif))
-            .foregroundColor(.green)
-            .underline(true, color: .red)
+            .font(.custom("Palatino", size: 25))
+            .foregroundColor(.blue)
             .fontWeight(.bold)
-            .italic()
+            .multilineTextAlignment(.center)
+            .lineLimit(nil)
+            .truncationMode(.middle)
+            .lineSpacing(8)
+            .padding()
+        //  .rotationEffect(.degrees(15), anchor: UnitPoint(x: 0, y: 0))
+            .rotation3DEffect(.degrees(50), axis: (x: 1, y: 0, z: 0))
+            .shadow(color: .gray, radius: 1, x: 5, y: 5)
     }
 }
 
