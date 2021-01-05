f5308af383cfadb4ae3ea27efbbc1d1e04fc3929
Jaime <jaime.castro5075@gmail.com>
2021-01-04 21:11:17 -0600

    Inicio de la App 07

---
+++ 07-draw/07-draw/CurveView.swift
@@ -0,0 +1,36 @@
+//
+//  CurveView.swift
+//  07-draw
+//
+//  Created by Lorenzo Jaime Castro Vazquez1 on 04/01/21.
+//  Copyright © 2021 Lorenzo Jaime Castro Vazquez1. All rights reserved.
+//
+
+import SwiftUI
+
+struct CurveView: View {
+    var body: some View {
+        ZStack{
+            Path(){ path in
+                path.move(to: CGPoint(x: 60, y: 130))
+                path.addLine(to: CGPoint(x: 130, y: 60))
+                path.addLine(to: CGPoint(x: 200, y: 130))
+            }
+            Path(){ path in
+                path.move(to: CGPoint(x:30, y:130))
+                path.addLine(to: CGPoint(x: 60, y: 130))
+                path.addQuadCurve(to: CGPoint(x: 200, y: 130), control: CGPoint(x: 130, y: 60))
+                path.addLine(to: CGPoint(x: 230, y: 130))
+                path.addLine(to: CGPoint(x: 230, y: 250))
+                path.addLine(to: CGPoint(x: 30, y: 250))
+                path.closeSubpath()
+            }.fill(Color.green)
+     }
+    }
+}
+
+struct CurveView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurveView()
+    }
+}
