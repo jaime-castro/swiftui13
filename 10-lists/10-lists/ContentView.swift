00b0e1abb236931ee02677fdcdf284bd35548ffb
Jaime <jaime.castro5075@gmail.com>
2021-01-12 20:54:52 -0600

    Tarea app 10

--- 10-lists/10-lists/ContentView.swift
+++ 10-lists/10-lists/ContentView.swift
@@ -11,27 +11,30 @@ import SwiftUI
 struct ContentView: View {
     
     var planes = [
-        Plane(name: "Airbus 320", image: "A320"),
-        Plane(name: "Airbus 330", image: "A330"),
-        Plane(name: "Airbus 340", image: "A340"),
-        Plane(name: "Airbus 350", image: "A350"),
-        Plane(name: "Airbus 380", image: "A380"),
-        Plane(name: "Antonov 225", image: "AN225"),
-        Plane(name: "Boeing 727", image: "B727"),
-        Plane(name: "Boeing 737", image: "B737"),
-        Plane(name: "Boeing 747", image: "B747"),
-        Plane(name: "Boeing 757", image: "B757"),
-        Plane(name: "Boeing 767", image: "B767"),
-        Plane(name: "Boeing 777", image: "B777"),
-        Plane(name: "Boeing 787", image: "B787")
+        Plane(name: "Antonov 225     (84 m)",   image: "AN225"),
+        Plane(name: "Airbus 380-900  (79.4 m)", image: "A380"),
+        Plane(name: "Boeing 747-800  (76.4 m)", image: "B747"),
+        Plane(name: "Airbus 340-600  (75.3 m)", image: "A340"),
+        Plane(name: "Boeing 777-300  (73.9 m)", image: "B777"),
+        Plane(name: "Airbus 350-1000 (73.88 m)",image: "A350"),
+        Plane(name: "Boeing 787-10   (68.3 m)", image: "B787"),
+        Plane (name: "Airbus 330-300 (63.6 m)", image: "A330"),
+        Plane(name: "Boeing 767-400  (61.4 m)", image: "B767"),
+        Plane(name: "Boeing 757      (54.5 m)", image: "B757"),
+        Plane(name: "Boeing 727      (46.7 m)", image: "B727"),
+        Plane(name: "Boeing 737-900  (42.1 m)", image: "B737"),
+        Plane(name: "Airbus 320      (37.57 m)",image: "A320")
     ]
     
     var body: some View {
-        
+        VStack{
+            Text("Lista de aviones del mas grande al mas pequeño")
         List(planes) { plane in
             PlaneRow(plane: plane)
         }
+        }
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
